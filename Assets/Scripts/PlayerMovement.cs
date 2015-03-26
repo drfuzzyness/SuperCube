9611ca5f6bfe24bfcce7e48331c3e2a969768f65
Matthew Conto <mbsconto@gmail.com>
2015-03-26 13:05:57 -0400

    Basic tile generation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[Header("Balance")]
+	public float maxSpeed;
+
+	[Header("Setup")]
+	public float distanceBetweenLanes;
+
+	private float currentSpeed;
+	private int currentLane;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter(Collision col) {
+		// gameover
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetKeyDown( KeyCode.A ) ) {
+			// move left
+		} else if( Input.GetKeyDown( KeyCode.D ) ) { 
+			// move right
+		}	else if( Input.GetKeyDown( KeyCode.Space ) ) {
+			// space?
+		}
+	}
+}
---
+++ Assets/Scripts/RoadManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadManager : MonoBehaviour {
+
+	public PlayerMovement player;
+	[Header("Balance")]
+	public float drawDistance;
+	public float enemyChance;
+
+	[Header("Assets")]
+	public GameObject roadTile;
+	public Vector3 roadTileSize;
+	public GameObject enemyObject;
+
+	private Vector3 previousPosition;
+
+	// Use this for initialization
+	void Start () {
+		previousPosition = roadTile.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( previousPosition.z < player.transform.position.z + drawDistance ) {
+			generateTile();
+		}
+	}
+
+	void generateTile() {
+		Instantiate( roadTile, previousPosition + roadTileSize, roadTile.transform.rotation );
+		previousPosition.z += roadTileSize.z;
+	}
+}
